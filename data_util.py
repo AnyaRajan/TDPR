ae510ee34d82ae3782062b646a0e934a2ed88b23
Anya Rajan <135256087+AnyaRajan@users.noreply.github.com>
2025-03-24 01:50:10 +0530

    Update data_util.py

--- data_util.py
+++ data_util.py
@@ -25,6 +25,18 @@ conf = OmegaConf.load('config.yaml')
 from nltk.corpus import stopwords
 import corrupted_text
 import dataclasses
+from torchvision import transforms
+from PIL import Image
+
+def get_augmentation_pipeline():
+    return transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010))
+    ])
 
 @dataclasses.dataclass
 class CorruptionWeights:
